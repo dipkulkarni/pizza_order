d81d2ba0c3c94ea637a252d2143be0698e22c6c6
dipkulkarni <35924899+dipkulkarni@users.noreply.github.com>
2018-01-30 13:31:47 +0530

    Update order.php

--- server/order.php
+++ server/order.php
@@ -1,8 +1,13 @@
 <?php
 error_reporting(0);
 	if($_POST['data'] == 'success'){
-		echo "You have successfully placed your order and it will be delivered in 30 minutes";
+		echo "Thank You for your order. \n Your order has been successfully placed and it will be delivered in 30 minutes";
 	}else{
-		$order = $_POST['order']; 
+		$order = $_POST['order']['pizza_name'];
 	}
+	$order = $_POST['order'];
+	$obj->out = json_encode($order);
+	$myfile = fopen("order.txt", "w");
+	file_put_contents("order.txt", serialize($obj));
+	fclose($myfile);
 ?>
