8619b48add8daead8529b24ff6f2011a82b7da51
dipkulkarni <35924899+dipkulkarni@users.noreply.github.com>
2018-01-30 11:25:55 +0530

    Update order.php

--- server/order.php
+++ server/order.php
@@ -1,4 +1,8 @@
 <?php
-	$order = $_POST['order']; 
-	echo "You have successfully placed your order and it will be delivered in 30 minutes";
+error_reporting(0);
+	if($_POST['data'] == 'success'){
+		echo "You have successfully placed your order and it will be delivered in 30 minutes";
+	}else{
+		$order = $_POST['order']; 
+	}
 ?>
